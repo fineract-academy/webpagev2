3aaee4937637a91714d127634682a9e5f428107d
Adam Saghy <adamsaghy@gmail.com>
2024-11-20 19:32:22 +0100

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,6 @@
 jtd.onReady(function(){
     document.querySelectorAll(".nav-list-item").forEach(function (value, key, listObj) { value.classList.add("active")});
+    fetchContributionCounter();
 });
 
 function fetchContributionCounter() {
